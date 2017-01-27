6160fa979ac2ef7251b369343815ad233aa5c5df
JeroenSt <jst@dse.nl>
2017-01-27 10:04:32 +0100

    Startcasaan is now working

--- startcasaan.sh
+++ startcasaan.sh
@@ -1,4 +1,15 @@
-nohup php smartmeter/smartmeter.php&
-nohub watermeter/watermeter&
-nohub temperature/temperature.php&
-nohub server/casaanserver.php&
+kill `ps aux | grep casaanserver.php | awk '{print $2}'`
+
+sudo kill `ps aux | grep smartmeter.php | awk '{print $2}'`
+nohup sudo php smartmeter/smartmeter.php >smartmeter.log 2>&1 &
+
+sudo kill `ps aux | grep watermeter | awk '{print $2}'`
+nohup sudo watermeter/watermeter >watermeter.log 2>&1 &
+
+sudo kill `ps aux | grep temperature.php | awk '{print $2}'`
+nohup sudo php temperature/temperature.php >temperature.log 2>&1 &
+
+sudo kill `ps aux | grep sunelectricity.php | awk '{print $2}'`
+nohup sudo php sunelectricity/sunelectricity.php >sunelectricity.log 2>&1 &
+
+nohup php server/casaanserver.php >casaanserver.log 2>&1 &
