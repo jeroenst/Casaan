3a12b12005bf6d3af3b6ad05764511bd40f9d37f
Jeroen <jst@dse.nl>
2017-09-27 10:10:05 +0200

    Added scriptpath to logfiles path

--- startcasaan.sh
+++ startcasaan.sh
@@ -8,18 +8,18 @@ cd $SCRIPTPATH
 sudo kill `ps aux | grep casaanserver.php | awk '{print $2}'`
 
 sudo kill `ps aux | grep smartmeter.php | awk '{print $2}'`
-nohup sudo php $SCRIPTPATH/smartmeter/smartmeter.php $SCRIPTPATH/smartmeter/smartmeter.conf >smartmeter.log 2>&1 &
+nohup sudo php $SCRIPTPATH/smartmeter/smartmeter.php $SCRIPTPATH/smartmeter/smartmeter.conf >$SCRIPTPATH/smartmeter.log 2>&1 &
 
 sudo kill `ps aux | grep watermeter | awk '{print $2}'`
-nohup sudo $SCRIPTPATH/watermeter/watermeter $SCRIPTPATH/watermeter/watermeter.conf >watermeter.log 2>&1 &
+nohup sudo $SCRIPTPATH/watermeter/watermeter $SCRIPTPATH/watermeter/watermeter.conf >$SCRIPTPATH/watermeter.log 2>&1 &
 
 sudo kill `ps aux | grep temperature.php | awk '{print $2}'`
-nohup sudo php $SCRIPTPATH/temperature/temperature.php >temperature.log 2>&1 &
+nohup sudo php $SCRIPTPATH/temperature/temperature.php >$SCRIPTPATH/temperature.log 2>&1 &
 
 sudo kill `ps aux | grep sunelectricity.php | awk '{print $2}'`
-nohup sudo php $SCRIPTPATH/sunelectricity/sunelectricity.php >sunelectricity.log 2>&1 &
+nohup sudo php $SCRIPTPATH/sunelectricity/sunelectricity.php >$SCRIPTPATH/sunelectricity.log 2>&1 &
 
 sudo kill `ps aux | grep casaanzwave | awk '{print $2}'`
-nohup sudo $SCRIPTPATH/zwave/casaanzwave  >zwave.log 2>&1 &
+nohup sudo $SCRIPTPATH/zwave/casaanzwave  >$SCRIPTPATH/zwave.log 2>&1 &
 
-nohup php $SCRIPTPATH/server/casaanserver.php >casaanserver.log 2>&1 &
+nohup php $SCRIPTPATH/server/casaanserver.php >$SCRIPTPATH/casaanserver.log 2>&1 &
