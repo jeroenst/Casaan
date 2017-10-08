f75792ea37e2664c72f76e04a3148faf834eddec
Jeroen <jst@dse.nl>
2017-10-08 11:23:27 +0200

    casaan.ini is now located in /etc

--- startcasaan.sh
+++ startcasaan.sh
@@ -8,18 +8,18 @@ cd $SCRIPTPATH
 sudo kill `ps aux | grep casaanserver.php | awk '{print $2}'`
 
 sudo kill `ps aux | grep smartmeter.php | awk '{print $2}'`
-nohup sudo php $SCRIPTPATH/smartmeter/smartmeter.php $SCRIPTPATH/smartmeter/smartmeter.conf >$SCRIPTPATH/smartmeter.log 2>&1 &
+nohup sudo php $SCRIPTPATH/smartmeter/smartmeter.php /etc/casaan.ini >$SCRIPTPATH/smartmeter.log 2>&1 &
 
 sudo kill `ps aux | grep watermeter | awk '{print $2}'`
-nohup sudo $SCRIPTPATH/watermeter/watermeter $SCRIPTPATH/watermeter/watermeter.conf >$SCRIPTPATH/watermeter.log 2>&1 &
+nohup sudo $SCRIPTPATH/watermeter/watermeter /etc/casaan.ini >$SCRIPTPATH/watermeter.log 2>&1 &
 
 sudo kill `ps aux | grep temperature.php | awk '{print $2}'`
-nohup sudo php $SCRIPTPATH/temperature/temperature.php >$SCRIPTPATH/temperature.log 2>&1 &
+nohup sudo php $SCRIPTPATH/temperature/temperature.php /etc/casaan.ini >$SCRIPTPATH/temperature.log 2>&1 &
 
 sudo kill `ps aux | grep sunelectricity.php | awk '{print $2}'`
-nohup sudo php $SCRIPTPATH/sunelectricity/sunelectricity.php >$SCRIPTPATH/sunelectricity.log 2>&1 &
+nohup sudo php $SCRIPTPATH/sunelectricity/sunelectricity.php /etc/casaan.ini >$SCRIPTPATH/sunelectricity.log 2>&1 &
 
 sudo kill `ps aux | grep casaanzwave | awk '{print $2}'`
-nohup sudo $SCRIPTPATH/zwave/casaanzwave  >$SCRIPTPATH/zwave.log 2>&1 &
+nohup sudo $SCRIPTPATH/zwave/casaanzwave /etc/casaan.ini >$SCRIPTPATH/zwave.log 2>&1 &
 
-nohup php $SCRIPTPATH/server/casaanserver.php >$SCRIPTPATH/casaanserver.log 2>&1 &
+nohup php $SCRIPTPATH/server/casaanserver.php /etc/casaan.ini >$SCRIPTPATH/casaanserver.log 2>&1 &
